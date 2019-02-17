b7931b1abdad4ad9db958ead91dd48d74c8b2447
willxs <willxs@users.noreply.github.com>
2019-02-17 00:45:16 -0500

    update login js

---
+++ frontend/html/login.js
@@ -0,0 +1,10 @@
+function handleSubmitButton(){
+    let email = document.getElementById('inputEmail').value
+    let password =document.getElementById('inputPassword').value
+    //create a job and add it to data base
+    console.log(email)
+    console.log(password)
+
+    //CreateJob(user,title,adress,country,description,price)
+    //window.location.replace("/main.html");
+}
