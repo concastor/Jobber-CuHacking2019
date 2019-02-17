22dfe607ebde11ca9c8333cecfb3d71e18ca4f69
willxs <willxs@users.noreply.github.com>
2019-02-17 03:26:02 -0500

    dynamic card

--- frontend/html/main.js
+++ frontend/html/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    console.log("JUST LOADED")
+    let jobPost  = ''
+
+    jobPost += '<div class="d-flex align-items-center p-2 my-2 text-black-50 bg-white rounded box-shadow"><div class="col-sm-2 y-3 pl-2 col-md-2 sidebar"><p class="media-body  mb-0 small border-gray">CATEGORY</p></div><div class="col-md-8 px-1 "><p class="media-body pl-3 pb-3 mb-0 small lh-125 border-right border-left border-gray"><strong class="h6 d-block text-gray-dark">Title</strong>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p></div><div class="col-*-* pr-3 y-3"></div><button type="button" class="btn btn-light btn-large">Accept</button></div>'
+    jobPost += jobPost = '<div class="d-flex align-items-center p-2 my-2 text-black-50 bg-white rounded box-shadow"><div class="col-sm-2 y-3 pl-2 col-md-2 sidebar"><p class="media-body  mb-0 small border-gray">CATEGORY</p></div><div class="col-md-8 px-1 "><p class="media-body pl-3 pb-3 mb-0 small lh-125 border-right border-left border-gray"><strong class="h6 d-block text-gray-dark">Title</strong>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p></div><div class="col-*-* pr-3 y-3"></div><button type="button" class="btn btn-light btn-large">Accept</button></div>' 
+    jobPost +=jobPost = '<div class="d-flex align-items-center p-2 my-2 text-black-50 bg-white rounded box-shadow"><div class="col-sm-2 y-3 pl-2 col-md-2 sidebar"><p class="media-body  mb-0 small border-gray">CATEGORY</p></div><div class="col-md-8 px-1 "><p class="media-body pl-3 pb-3 mb-0 small lh-125 border-right border-left border-gray"><strong class="h6 d-block text-gray-dark">Title</strong>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p></div><div class="col-*-* pr-3 y-3"></div><button type="button" class="btn btn-light btn-large">Accept</button></div>' 
+
   
+    let foodDisplay = document.getElementById("p1")
+    foodDisplay.innerHTML = '' //reset whatever is in here
+    foodDisplay.innerHTML = jobPost; //set the formatedRaw data to the inner html
+              
+
   })
   
